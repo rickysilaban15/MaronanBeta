aa664514e862dec34b40f2913c70d3af4da32cbd
rickysilaban15 <rickysilaban384@gmail.com>
2025-07-07 13:23:24 +0700

    Update produk.ts

--- lib/produk.ts
+++ lib/produk.ts
@@ -1,14 +1,14 @@
-import { prisma } from "@/lib/prisma";
+// lib/produk.ts
+import { prisma } from "@/lib/prisma"
 
 export async function getAllProdukIds() {
-  const produkList = await prisma.produk.findMany({
+  return await prisma.produk.findMany({
     select: { id: true },
-  });
-  return produkList.map((produk) => produk.id);
+  })
 }
 
 export async function getProdukById(id: string) {
   return await prisma.produk.findUnique({
     where: { id },
-  });
+  })
 }
