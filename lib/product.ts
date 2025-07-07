a4f8d8454c11be23bf1d4ca614ae4f76e282370e
rickysilaban15 <rickysilaban384@gmail.com>
2025-07-07 13:04:03 +0700

    Update product.ts

--- lib/product.ts
+++ lib/product.ts
@@ -1,10 +1,7 @@
-// lib/produk.ts
-import { prisma } from "@/lib/prisma" // jika kamu pakai Prisma
-
 export async function getAllProdukIds() {
-  const produk = await prisma.produk.findMany({
-    select: { id: true },
-  })
-
-  return produk
+  return [
+    { id: "1" },
+    { id: "2" },
+    { id: "3" },
+  ]
 }
